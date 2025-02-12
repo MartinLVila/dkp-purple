3c8aa8c2c52f1633a7d89c271d28fe25956dd407
Martin Vila <48536243+MartinLVila@users.noreply.github.com>
2025-02-11 23:33:02 -0300

    change to tasks

--- tasks.py
+++ tasks.py
@@ -3,8 +3,7 @@ from datetime import datetime, timedelta
 from discord.ext import tasks
 
 import data_manager
-from data_manager import events_info, user_data
-from data_manager import guardar_eventos, guardar_datos
+from data_manager import events_info, user_data, guardar_eventos, guardar_datos
 
 logger = logging.getLogger('bot_tasks')
 
@@ -19,33 +18,32 @@ async def limpiar_eventos_expirados():
         for evento in eventos_a_eliminar:
             del events_info[evento]
             logger.info(f"Evento '{evento}' eliminado por limpieza de eventos expirados.")
-        guardar_eventos()
+        await guardar_eventos()
 
 @tasks.loop(minutes=10)
 async def limpiar_absences_expiradas():
     ahora = datetime.utcnow()
     modificados = False
     for nombre, datos in user_data.items():
-        if "absence_until" in datos and datos["absence_until"]:
+        if datos.get("absence_until"):
             if ahora > datos["absence_until"]:
                 user_data[nombre]["absence_until"] = None
                 modificados = True
                 logger.info(f"Ausencia de '{nombre}' ha expirado (limpiada).")
     if modificados:
-        guardar_datos()
+        await guardar_datos()
 
 @tasks.loop(minutes=10)
 async def limpiar_eventos_justificados_expirados():
     ahora = datetime.utcnow()
     modificados = False
     for nombre_evento, info in list(events_info.items()):
-        evento_time = info["timestamp"]
-        if ahora > evento_time + timedelta(minutes=60):
+        if ahora > info["timestamp"] + timedelta(minutes=60):
             del events_info[nombre_evento]
             modificados = True
             logger.info(f"Evento '{nombre_evento}' eliminado por limpieza.")
     if modificados:
-        guardar_eventos()
+        await guardar_eventos()
 
 def iniciar_tareas(bot):
     limpiar_eventos_expirados.start()
